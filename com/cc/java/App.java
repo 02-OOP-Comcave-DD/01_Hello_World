ba726e07d3ff6abdeef015517f11b6ffb213b7c1
Rene <media-r.r@online.de>
2024-05-21 12:49:11 +0200

    rumgespielt | einige Kommentare hinzu gefügt

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,10 +2,12 @@ package com.cc.java;
 
 public class App 
 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+public static void main(String[] args) {                     // puplic (öffentlich)|static (etwas festes)|void (etwas leeres(kein Return))|main (Einstiegspunkt in Java)
+                                                             // String (Datentyp) (Java arbeitet mit Datentypen (Zahlen, String, Boolean, ...))
+    System.out.println("Hi Welt, was geht ab?!");
+    System.out.println("Hey, Mistkerl, Hasta´la Vista");     // Kurzbefehl für diese Zeile "syso"
+}
+    
     
 }
 
